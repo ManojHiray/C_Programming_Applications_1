48445985bc1db643a38dddc47f464eb101f96e14
Manoj Hiray <80141438+ManojHiray@users.noreply.github.com>
2022-02-10 18:03:51 +0530

    Update LB4.2.c

--- LB4.2.c
+++ LB4.2.c
@@ -1,3 +1,14 @@
+/*
+2.Write a program which accept number from user and display its factors in
+decreasing order.
+Input : 12
+Output : 6 4 3 2 1
+Input : 13
+Output : 1
+Input : 10
+Output : 5 2 1
+*/
+
 #include<stdio.h>
 void FactRev(int iNo)
 {
