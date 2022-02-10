ddb5a539801a4003484eb9d900c755c5d041e47c
Manoj Hiray <80141438+ManojHiray@users.noreply.github.com>
2022-02-10 17:50:36 +0530

    Update LB3.1.c

--- LB3.1.c
+++ LB3.1.c
@@ -1,3 +1,10 @@
+/*
+1.Write a program which accept one number from user and print that number of
+even numbers on screen.
+Input : 7
+Output: 2 4 6 8 10 12 14
+*/
+
 #include<stdio.h>
 void PrintEven(int iNo)
 {
