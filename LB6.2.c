c58a3ed34998b304cf127bd547063a63f448e3f4
Manoj Hiray <80141438+ManojHiray@users.noreply.github.com>
2022-02-10 18:16:03 +0530

    Update LB6.2.c

--- LB6.2.c
+++ LB6.2.c
@@ -1,3 +1,16 @@
+/*
+2.Write a program which accept number from user and return the count of odd
+digits.
+Input : 2395
+Output : 3
+Input : 1018
+Output : 2
+Input : -1018
+Output : 2
+Input : 8462
+Output : 0
+*/
+
 #include<stdio.h>
 
 int CountOdd(int iNo)
