7a383b6593df1ddbe510448c30c3dbe03800422a
Manoj Hiray <80141438+ManojHiray@users.noreply.github.com>
2022-02-10 19:02:50 +0530

    Update LB6.5.c

--- LB6.5.c
+++ LB6.5.c
@@ -1,3 +1,16 @@
+/*
+5.Write a program which accept number from user and return difference between
+summation of even digits and summation of odd digits.
+Input : 2395
+Output : -15 (2 - 17)
+Input : 1018
+Output : 6 (8 - 2)
+Input : 8440
+Output : 16 (16 - 0)
+Input : 5733
+Output : -18 (0 - 18)
+*/
+
 #include<stdio.h>
 
 int CountDiff(int iNo)
