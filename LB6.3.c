b0616c8076acec3f3445c4e89a343fa2983c1601
Manoj Hiray <80141438+ManojHiray@users.noreply.github.com>
2022-02-10 19:00:41 +0530

    Update LB6.3.c

--- LB6.3.c
+++ LB6.3.c
@@ -1,3 +1,16 @@
+/*
+3.Write a program which accept number from user and return the count of digits in
+between 3 and 7.
+Input : 2395
+Output : 1
+Input : 1018
+Output : 0
+Input : 4521
+Output : 2
+Input : 9922
+Output : 0
+*/
+
 #include<stdio.h>
 
 int CountRange(int iNo)
