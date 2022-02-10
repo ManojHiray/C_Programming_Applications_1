f64a473a42eee48e94f9718910c057242f80dafb
Manoj Hiray <80141438+ManojHiray@users.noreply.github.com>
2022-02-10 19:01:41 +0530

    Update LB6.4.c

--- LB6.4.c
+++ LB6.4.c
@@ -1,3 +1,16 @@
+/*
+4.Write a program which accept number from user and return multiplication of all
+digits.
+Input : 2395
+Output : 270
+Input : 1018
+Output : 8
+Input : 9440
+Output : 144
+Input : 922432
+Output : 864
+*/
+
 #include<stdio.h>
 
 int MultDigits(int iNo)
