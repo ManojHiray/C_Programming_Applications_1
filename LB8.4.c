3843e5b71d2d95b93c656312fd2c5c35e896b661
Manoj Hiray <80141438+ManojHiray@users.noreply.github.com>
2022-02-10 19:11:54 +0530

    Update LB8.4.c

--- LB8.4.c
+++ LB8.4.c
@@ -1,3 +1,16 @@
+/*
+4.Write a program which accept range from user and return addition of all even
+numbers in between that range. (Range should contains positive numbers only)
+Input : 23 30
+Output : 108
+Input : 10 18
+Output : 70
+Input : -10 2
+Output : Invalid range
+Input : 90 18
+Output : Invalid range
+*/
+
 #include<stdio.h>
 
 int RangeSumEven(int iStart, int iEnd)
