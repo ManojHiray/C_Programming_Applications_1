8343b2c7f1d9d1208a7c04acecb4c024366ba922
Manoj Hiray <80141438+ManojHiray@users.noreply.github.com>
2022-02-10 17:57:32 +0530

    Update LB4.1.c

--- LB4.1.c
+++ LB4.1.c
@@ -1,8 +1,12 @@
 /*
-iNo 12
-iMult 144
-iCnt 
-
+1.Write a program which accept number from user and display its multiplication of
+factors.
+Input : 12
+Output : 144 (1 * 2 * 3 * 4 * 6)
+Input : 13
+Output : 1 (1)
+Input : 10
+Output : 10 (1 * 2 * 5)
 */
 
 #include<stdio.h>
