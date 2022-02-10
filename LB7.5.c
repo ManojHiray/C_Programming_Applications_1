e48c5fdd8fed7b332df4a6c323e572372dfe3a5a
Manoj Hiray <80141438+ManojHiray@users.noreply.github.com>
2022-02-10 19:07:06 +0530

    Update LB7.5.c

--- LB7.5.c
+++ LB7.5.c
@@ -1,3 +1,12 @@
+/*
+5. Write a program which accept area in square feet and convert it into square
+meter. (1 square feet = 0.0929 Square meter)
+Input : 5
+Output : 0.464515
+Input : 7
+Output : 0.650321
+*/
+
 #include<stdio.h>
 
 double SquareMeter(int iValue)
