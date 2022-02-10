47a8e5a457c5553e4b12f524ff3b4cdf90b2bf85
Manoj Hiray <80141438+ManojHiray@users.noreply.github.com>
2022-02-10 19:11:13 +0530

    Update LB8.3.c

--- LB8.3.c
+++ LB8.3.c
@@ -1,3 +1,16 @@
+/*
+3. Write a program which accept range from user and return addition of all numbers
+in between that range. (Range should contains positive numbers only)
+Input : 23 30
+Output : 212
+Input : 10 18
+Output : 126
+Input : -10 2
+Output : Invalid range
+Input : 90 18
+Output : Invalid range
+*/
+
 
 #include<stdio.h>
 
