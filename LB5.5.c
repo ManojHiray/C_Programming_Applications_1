8e316570cdf32fa4dc8edb5240bd56e92491a9f7
Manoj Hiray <80141438+ManojHiray@users.noreply.github.com>
2022-02-10 18:13:54 +0530

    Update LB5.5.c

--- LB5.5.c
+++ LB5.5.c
@@ -1,3 +1,16 @@
+/*
+5.Write a program which accept number from user and count frequency of such a
+digits which are less than 6.
+Input : 2395
+Output : 3
+Input : 1018
+Output : 3
+Input : 9440
+Output : 3
+Input : 96672
+Output : 1
+*/
+
 #include<stdio.h>
 
 int Count(int iNo)
