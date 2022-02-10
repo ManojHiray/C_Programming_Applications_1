e68f3b4027391d517663c9e77d0cae7ee52ab69b
Manoj Hiray <80141438+ManojHiray@users.noreply.github.com>
2022-02-10 19:06:08 +0530

    Update LB7.3.c

--- LB7.3.c
+++ LB7.3.c
@@ -1,3 +1,12 @@
+/*
+3. Write a program which accept distance in kilometre and convert it into meter. (1
+kilometre = 1000 Meter)
+Input : 5
+Output : 5000
+Input : 12
+Output : 12000
+*/
+
 #include<stdio.h>
 
 int KMtoMeter(int iNo)
