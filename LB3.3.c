687432806fab131b46ae78f10dbd2595e7c01464
Manoj Hiray <80141438+ManojHiray@users.noreply.github.com>
2022-02-10 17:52:57 +0530

    Update LB3.3.c

--- LB3.3.c
+++ LB3.3.c
@@ -1,3 +1,10 @@
+/*
+3. Write a program which accept number from user and print even factors of that
+number.
+Input : 36
+Output: 2 6 12 18
+*/
+
 #include<stdio.h>
 void DisplayEvenFactor(int iNo)
 {
