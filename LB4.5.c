9230423d59e42b23d039bf94a250df91cfbf36fd
Manoj Hiray <80141438+ManojHiray@users.noreply.github.com>
2022-02-10 18:05:30 +0530

    Update LB4.5.c

--- LB4.5.c
+++ LB4.5.c
@@ -1,3 +1,12 @@
+/*
+5.Write a program which accept number from user and return difference between
+summation of all its factors and non factors.
+Input : 12
+Output : -34 (16 - 50)
+Input : 10
+Output : -29 (8 - 37)
+*/
+
 #include<stdio.h>
 
 int FactDiff(int iNo)
