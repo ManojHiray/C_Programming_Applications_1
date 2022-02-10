a022d6b6822a79fbb0f1bd3ff780eb8502c80113
Manoj Hiray <80141438+ManojHiray@users.noreply.github.com>
2022-02-10 19:07:48 +0530

    Update LB7.4.c

--- LB7.4.c
+++ LB7.4.c
@@ -1,3 +1,12 @@
+/*
+4. Write a program which accept temperature in Fahrenheit and convert it into
+celsius. (1 celsius = (Fahrenheit -32) * (5/9))
+Input : 10
+Output : -12.2222 (10 - 32) * (5/9)
+Input : 34
+Output : 1.11111 (34 - 32) * (5/9)
+*/
+
 #include<stdio.h>
 
 double FhtoCs(float fTemp)
