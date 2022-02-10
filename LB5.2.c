ab2b7e93db1c4b615f9d6a0f4fa7c177075b0bfd
Manoj Hiray <80141438+ManojHiray@users.noreply.github.com>
2022-02-10 18:10:36 +0530

    Update LB5.2.c

--- LB5.2.c
+++ LB5.2.c
@@ -1,3 +1,16 @@
+/*
+2.Write a program which accept number from user and check whether it contains 0
+in it or not.
+Input : 2395
+Output : There is no Zero
+Input : 1018
+Output : It Contains Zero
+Input : 9000
+Output : It Contains Zero
+Input : 10687
+Output : It Contains Zero
+*/
+
 #include<stdio.h>
 
 #define TRUE 1
